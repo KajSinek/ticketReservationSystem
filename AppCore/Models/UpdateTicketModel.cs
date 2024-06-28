a37ed297c5d84948c80141f1ff68a1e6ae001fc9
Tibor Krátky <46248669+KajSinek@users.noreply.github.com>
2024-06-28 13:18:37 +0200

    controllers + fixing
    Controllers with not implemented exception
    Missing models

---
+++ AppCore/Models/UpdateTicketModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AppCore.Models;
+
+public class UpdateTicketModel
+{
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
+    public DateOnly ExpirationDate { get; set; }
+    public DateOnly ValidityStartDate { get; set; }
+}
