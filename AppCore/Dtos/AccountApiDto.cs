670caa03a395a8692f00146a2410ea7db2c1b652
Tibor Krátky <46248669+KajSinek@users.noreply.github.com>
2024-06-28 12:46:33 +0200

    ticket dto + model

--- AppCore/Dtos/AccountApiDto.cs
+++ AppCore/Dtos/AccountApiDto.cs
@@ -2,23 +2,13 @@
 
 namespace AppCore.Dtos;
 
-public abstract class AccountApiDto
+public abstract class AccountApiDto(Account account)
 {
-    protected AccountApiDto(Account account)
-    {
-        AccountId = account.AccountId;
-        Username = account.Username;
-        Email = account.Email;
-        FirstName = account.FirstName;
-        LastName = account.LastName;
-        PhoneNumber = account.PhoneNumber;
-        Address = account.Address;
-    }
-    public Guid AccountId { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Address { get; set; }
+    public Guid AccountId { get; set; } = account.AccountId;
+    public string Username { get; set; } = account.Username;
+    public string Email { get; set; } = account.Email;
+    public string FirstName { get; set; } = account.FirstName;
+    public string LastName { get; set; } = account.LastName;
+    public string PhoneNumber { get; set; } = account.PhoneNumber;
+    public string Address { get; set; } = account.Address;
 }
---
+++ AppCore/Dtos/TicketApiDto.cs
@@ -0,0 +1,12 @@
+﻿using AppCore.Entities;
+
+namespace AppCore.Dtos;
+
+public class TicketApiDto(Ticket ticket)
+{
+    public Guid TicketId { get; set; } = ticket.TicketId;
+    public string Name { get; set; } = ticket.Name;
+    public decimal Price { get; set; } = ticket.Price;
+    public DateOnly ExpirationDate { get; set; } = ticket.ExpirationDate;
+    public DateOnly ValidityStartDate { get; set; } = ticket.ValidityStartDate;
+}
